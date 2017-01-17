c9b6b72de8509ab559777bf9bcddba9ae43b6b81
Patrick McKinnon <patrick@ojolabs.com>
2017-01-17 14:13:56 -0600

    - Hack Virtual Dimmer to Virtual Switch

--- devicetypes/michaelstruck/virtual-dimmer.src/virtual-dimmer.groovy
+++ devicetypes/michaelstruck/virtual-dimmer.src/virtual-dimmer.groovy
@@ -6,7 +6,7 @@
  *
  *  Version 1.0.0 - Initial release
  *  Version 1.1.0 - Updated the interface to better match SmartThings dimmers (thanks to @BoruGee)
- * 
+ *
  *  Uses code from SmartThings
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
@@ -21,84 +21,48 @@
  */
 
 metadata {
-        definition (name: "Virtual Dimmer", namespace: "MichaelStruck", author: "SmartThings") {
+        definition (name: "Virtual Switch", namespace: "pmckinnon", author: "SmartThings") {
         capability "Switch"
         capability "Refresh"
-        capability "Switch Level"
     }
 
-	// simulator metadata
-	simulator {
-	}
+    // simulator metadata
+    simulator {
+    }
 
-	// UI tile definitions
-	tiles(scale: 2) {
-		multiAttributeTile(name: "switch", type: "lighting", width: 6, height: 4, canChangeIcon: true, canChangeBackground: true) {
-			tileAttribute("device.switch", key: "PRIMARY_CONTROL") {
-    			attributeState "off", label: '${name}', action: "switch.on", icon: "st.switches.light.off", backgroundColor: "#ffffff", nextState: "turningOn"
-		      	attributeState "on", label: '${name}', action: "switch.off", icon: "st.switches.light.on", backgroundColor: "#79b821", nextState: "turningOff"
-				attributeState "turningOff", label: '${name}', action: "switch.on", icon: "st.switches.light.off", backgroundColor: "#ffffff", nextState: "turningOn"
-		      	attributeState "turningOn", label: '${name}', action: "switch.off", icon: "st.switches.light.on", backgroundColor: "#79b821", nextState: "turningOff"
-        	}
-        		tileAttribute("device.level", key: "SLIDER_CONTROL") {
-            		attributeState "level", action:"switch level.setLevel"
-        		}
-        		tileAttribute("level", key: "SECONDARY_CONTROL") {
-              		attributeState "level", label: 'Light dimmed to ${currentValue}%'
-        		}    
-		}
-        valueTile("lValue", "device.level", inactiveLabel: true, height:2, width:2, decoration: "flat") {  
-			state "levelValue", label:'${currentValue}%', unit:"", backgroundColor: "#53a7c0"  
-        }  
-    
-		standardTile("refresh", "device.switch", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
-			state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
-		}
-		main "switch"
-		details(["switch","lValue","refresh"])
+    // UI tile definitions
+    tiles(scale: 2) {
+        multiAttributeTile(name: "switch", type: "lighting", width: 6, height: 4, canChangeIcon: true, canChangeBackground: true) {
+            tileAttribute("device.switch", key: "PRIMARY_CONTROL") {
+                attributeState "off", label: '${name}', action: "switch.on", icon: "st.switches.light.off", backgroundColor: "#ffffff", nextState: "turningOn"
+                  attributeState "on", label: '${name}', action: "switch.off", icon: "st.switches.light.on", backgroundColor: "#79b821", nextState: "turningOff"
+                attributeState "turningOff", label: '${name}', action: "switch.on", icon: "st.switches.light.off", backgroundColor: "#ffffff", nextState: "turningOn"
+                  attributeState "turningOn", label: '${name}', action: "switch.off", icon: "st.switches.light.on", backgroundColor: "#79b821", nextState: "turningOff"
+            }
+        }
 
-	}
+        standardTile("refresh", "device.switch", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+            state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
+        }
+        main "switch"
+        details(["switch","lValue","refresh"])
+
+    }
 }
 
 def parse(String description) {
 }
 
 def on() {
-	sendEvent(name: "switch", value: "on")
-    log.info "Dimmer On"
+    sendEvent(name: "switch", value: "on")
+    log.info "Virtual Switch On"
 }
 
 def off() {
-	sendEvent(name: "switch", value: "off")
-    log.info "Dimmer Off"
-}
-
-def setLevel(val){
-    log.info "setLevel $val"
-    
-    // make sure we don't drive switches past allowed values (command will hang device waiting for it to
-    // execute. Never commes back)
-    if (val < 0){
-    	val = 0
-    }
-    
-    if( val > 100){
-    	val = 100
-    }
-    
-    if (val == 0){ 
-    	sendEvent(name:"level",value:val)
-    	off()
-    }
-    else
-    {
-    	on()
-    	sendEvent(name:"level",value:val)
-    	sendEvent(name:"switch.setLevel",value:val)
-    }
+    sendEvent(name: "switch", value: "off")
+    log.info "Virtual Switch Off"
 }
 
 def refresh() {
     log.info "refresh"
 }
-
